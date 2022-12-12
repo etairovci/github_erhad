43160a643a6dcbf0fda07ba3d72a280ca91dbb2e
erhadtairovci <etairovci@gmail.com>
2022-12-12 00:46:55 -0500

    Erhad_proba_PrviDan

---
+++ src/ErhadGitHub/TijanuBoliStomak.java
@@ -0,0 +1,8 @@
+package ErhadGitHub;
+
+public class TijanuBoliStomak {
+    public static void main(String[] args) {
+
+        System.out.println("Tijanu Boli stomak" + "\nJelena se kupa");
+    }
+}
