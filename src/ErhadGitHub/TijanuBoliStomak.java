3ed9545579ed2d1278c17bb8ca4756d25e85cd1b
etairovci <114275485+etairovci@users.noreply.github.com>
2022-12-12 00:48:20 -0500

    Update TijanuBoliStomak.java

--- src/ErhadGitHub/TijanuBoliStomak.java
+++ src/ErhadGitHub/TijanuBoliStomak.java
@@ -4,5 +4,7 @@ public class TijanuBoliStomak {
     public static void main(String[] args) {
 
         System.out.println("Tijanu Boli stomak" + "\nJelena se kupa");
+        
+        //probaj da downloadujes file
     }
 }
